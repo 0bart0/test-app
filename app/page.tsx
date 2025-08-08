9a37f20632223a1964849505d067344982f547cc
Bartosz <bkempa96@gmail.com>
2025-08-08 12:23:00 +0200

    dodanie linku do strony glownej

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ export default function Home() {
   return (
     <main>
       <h1>Witaj na mojej stronie</h1>
+      <p>
+        <a href="/score2">Przejdź do kalkulatora SCORE2</a>
+      </p>
+
     </main>
   )
 }
